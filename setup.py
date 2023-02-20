00c755d3bef3ff245432024024aba2db3c775c2d
dolf <rudolfbyker@gmail.com>
2023-02-20 12:09:19 +0200

    Exclude `test` folder from installed packages.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AutoActuary/aa-py-xl-convert",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["test"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Other/Proprietary License",
